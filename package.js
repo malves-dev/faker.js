e1893925bdb21c2c508993e3b4b2b96b4cd09030
Mario S. Alves <malves.dev@gmail.com>
2019-05-04 23:38:53 -0300

    Initial settings after fork.

--- package.js
+++ package.js
@@ -14,17 +14,13 @@ Package.describe({
 
 Package.onUse(function (api) {
   api.versionsFrom('1.0');
-
   api.addFiles('meteor/npm-require.js', 'server');
-
   api.addFiles('build/build/faker.js', 'client');
-
   api.export("faker", "server");
 });
 
 
 Package.onTest(function(api) {
   api.use(['practicalmeteor:faker', "coffeescript", "tinytest", 'practicalmeteor:chai']);
-
   api.addFiles("meteor/tests/faker-export-test.coffee");
 });
